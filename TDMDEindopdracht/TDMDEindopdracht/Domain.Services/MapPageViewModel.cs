4698bfbb25370d6043c79b06d079a9852c3939c3
Falcon.G2 <ctw.vanderpluijm@student.avans.nl>
2025-01-18 15:31:57 +0100

    Zoomed naar userlocation

--- TDMDEindopdracht/TDMDEindopdracht/Domain.Services/MapPageViewModel.cs
+++ TDMDEindopdracht/TDMDEindopdracht/Domain.Services/MapPageViewModel.cs
@@ -4,6 +4,7 @@ using Microsoft.Maui.Maps;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,13 +19,28 @@ namespace TDMDEindopdracht.Domain.Services
         public MapPageViewModel(IGeolocation location) 
         { 
             geolocation = location;
-            ZoomToBreda();
+            ZoomToUserLocation();
         }
-        private void ZoomToBreda()
+        private async void ZoomToUserLocation()
         {
-            Location location = new Location(51.588331, 4.777802);
-            MapSpan mapSpan = new MapSpan(location, 0.015, 0.015);
-            CurrentMapSpan = mapSpan;
+            try
+            {
+                var userLocation = await Geolocation.GetLastKnownLocationAsync();
+                if (userLocation != null)
+                {
+                    Location location = new Location(userLocation.Latitude, userLocation.Longitude);
+                    MapSpan mapSpan = new MapSpan(location, 0.015, 0.015);
+                    CurrentMapSpan = mapSpan;
+                }
+                else
+                {
+                    Debug.Write("De huidige locatie kon niet worden opgehaald");
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.Write(ex.ToString());
+            }
         }
     }
 
