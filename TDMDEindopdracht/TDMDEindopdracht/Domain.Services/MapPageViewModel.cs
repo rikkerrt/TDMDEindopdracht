ae456d25744e3ceea531cbca2ea40f336b297412
Rick <rrhvanhoof@gmail.com>
2025-01-19 13:45:28 +0100

    base for routes

--- TDMDEindopdracht/TDMDEindopdracht/Domain.Services/MapPageViewModel.cs
+++ TDMDEindopdracht/TDMDEindopdracht/Domain.Services/MapPageViewModel.cs
@@ -8,6 +8,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TDMDEindopdracht.Infrastructure;
 
 namespace TDMDEindopdracht.Domain.Services
 {
@@ -15,12 +16,13 @@ namespace TDMDEindopdracht.Domain.Services
     {
         [ObservableProperty] private MapSpan _currentMapSpan;
         [ObservableProperty] private ObservableCollection<MapElement> _mapElements= [];
-        [ObservableProperty] private ObservableCollection<Pin> _pins;
+        //[ObservableProperty] private ObservableCollection<MapElement> _pins;
         private readonly IGeolocation geolocation;
         public MapPageViewModel(IGeolocation location) 
         { 
             geolocation = location;
             ZoomToUserLocation();
+            createRoute();
         }
         private async void ZoomToUserLocation()
         {
@@ -44,6 +46,23 @@ namespace TDMDEindopdracht.Domain.Services
             }
         }
 
+        public void createRoute()
+        {
+            Polyline routeLine =  new Polyline
+            {
+                StrokeColor = Color.FromArgb("1F51FF"),
+                StrokeWidth = 1
+            };
+
+            List<Location> locations = APIManager.DecodePolyLine("knjmEnjunUbKCfEA?_@]@kMBeE@qIIoF@wH@eFFk@WOUI_@?u@j@k@`@EXLTZHh@Y`AgApAaCrCUd@cDpDuAtAoApA{YlZiBdBaIhGkFrDeCtBuFxFmIdJmOjPaChDeBlDiAdD}ApGcDxU}@hEmAxD}[tt@yNb\\\\yBdEqFnJqB~DeFxMgK~VsMr[uKzVoCxEsEtG}BzCkHhKWh@]t@{AxEcClLkCjLi@`CwBfHaEzJuBdEyEhIaBnCiF|K_Oz\\\\{MdZwAbDaKbUiB|CgCnDkDbEiE|FqBlDsLdXqQra@kX|m@aF|KcHtLm@pAaE~JcTxh@w\\\\`v@gQv`@}F`MqK`PeGzIyGfJiG~GeLhLgIpIcE~FsDrHcFfLqDzH{CxEwAbBgC|B}F|DiQzKsbBdeA{k@~\\\\oc@bWoKjGaEzCoEzEwDxFsUh^wJfOySx[uBnCgCbCoFlDmDvAiCr@eRzDuNxC_EvAiFpCaC|AqGpEwHzFoQnQoTrTqBlCyDnGmCfEmDpDyGzGsIzHuZzYwBpBsC`CqBlAsBbAqCxAoBrAqDdDcNfMgHbHiPtReBtCkD|GqAhBwBzBsG~FoAhAaCbDeBvD_BlEyM``@uBvKiA~DmAlCkA|B}@lBcChHoJnXcB`GoAnIS~CIjFDd]A|QMlD{@jH[vAk@`CoGxRgPzf@aBbHoB~HeMx^eDtJ}BnG{DhJU`@mBzCoCjDaAx@mAnAgCnBmAp@uAj@{Cr@wBPkB@kBSsEW{GV}BEeCWyAWwHs@qH?cIHkDXuDn@mCt@mE`BsH|CyAp@}AdAaAtAy@lBg@pCa@jE]fEcBhRq@pJKlCk@hLFrB@lD_@xCeA`DoBxDaHvM_FzImDzFeCpDeC|CkExDiJrHcBtAkDpDwObVuCpFeCdHoIl\\\\uBjIuClJsEvMyDbMqAhEoDlJ{C|J}FlZuBfLyDlXwB~QkArG_AnDiAxC{G|OgEdLaE`LkBbEwG~KgHnLoEjGgDxCaC`BuJdFkFtCgCnBuClD_HdMqEzHcBpB_C|BuEzCmPlIuE|B_EtDeBhCgAdCw@rCi@|DSfECrCAdCS~Di@jDYhA_AlC{AxCcL`U{GvM_DjFkBzBsB`BqDhBaEfAsTvEmEr@iCr@qDrAiFnCcEzCaE~D_@JmFdGQDwBvCeErEoD|BcFjC}DbEuD~D`@Zr@h@?d@Wr@}@vAgCbEaHfMqA`Cy@dAg@bAO`@gCi@w@W\"");
+            foreach (Location location in locations) {
+                Debug.WriteLine($"lat = {location.Latitude}, lng = {location.Longitude}");
+                routeLine.Geopath.Add(location);
+            }
+
+            MapElements.Add(routeLine);
+        }
+
         public void setPin(double lat, double lng, string longname)
         {
             Pin pin = new Pin
@@ -53,7 +72,7 @@ namespace TDMDEindopdracht.Domain.Services
                 Type = PinType.Place
             };
 
-            Pins.Add(pin);
+            //Pins.Add(pin);
         }
     }
 }
--- TDMDEindopdracht/TDMDEindopdracht/Infrastructure/APIManager.cs
+++ TDMDEindopdracht/TDMDEindopdracht/Infrastructure/APIManager.cs
@@ -20,7 +20,6 @@ namespace TDMDEindopdracht.Infrastructure
             Debug.WriteLine($"lat = {lat}");
             Debug.WriteLine($"Lng = {lng}");
 
-            //string apiLink= $"https://gateway.apiportal.ns.nl/nsapp-stations/v3/nearest?lat={lat}%lng={lng}&limit=2&includeNonPlannableStations=false";
             string apiLink= $"https://gateway.apiportal.ns.nl/nsapp-stations/v3/nearest?lat={lat}&lng={lng}&limit=2&includeNonPlannableStations=false";
             Debug.WriteLine(apiLink);
             
@@ -33,12 +32,68 @@ namespace TDMDEindopdracht.Infrastructure
             return jsondoc;
         }
 
+        public static List<Location> GetPolyLineList(Location location1, Location Location2)
+        {
+            
+            
+            return DecodePolyLine("");
+        }
+
 
         public static List<Location> DecodePolyLine(string encodedPolyLine)
         {
+            int index = 0;
+            var polylineChars = encodedPolyLine.ToCharArray();
+            var poly = new List<Location>();
+            int currentLat = 0;
+            int currentLng = 0;
+            int next5Bits;
+
+            while (index < polylineChars.Length)
+            {
+                // calculate next latitude
+                int sum = 0;
+                int shifter = 0;
+
+                do
+                {
+                    next5Bits = polylineChars[index++] - 63;
+                    sum |= (next5Bits & 31) << shifter;
+                    shifter += 5;
+                }
+                while (next5Bits >= 32 && index < polylineChars.Length);
 
 
-            return new List<Location>();
+
+                if (index >= polylineChars.Length)
+                {
+                    break;
+                }
+
+                currentLat += (sum & 1) == 1 ? ~(sum >> 1) : (sum >> 1);
+
+                // calculate next longitude
+                sum = 0;
+                shifter = 0;
+
+                do
+                { 
+                    next5Bits = polylineChars[index++] - 63;
+                    sum |= (next5Bits & 31) << shifter;
+                    shifter += 5;
+                }
+                while (next5Bits >= 32 && index < polylineChars.Length);
+
+                if (index >= polylineChars.Length && next5Bits >= 32)
+                {
+                    break;
+                }
+
+                currentLng += (sum & 1) == 1 ? ~(sum >> 1) : (sum >> 1);
+                Location mLatLng = new Location(Convert.ToDouble(currentLat) / 100000.0, Convert.ToDouble(currentLng) / 100000.0);
+                poly.Add(mLatLng);
+            }
+            return poly;
         }
     }
 }
